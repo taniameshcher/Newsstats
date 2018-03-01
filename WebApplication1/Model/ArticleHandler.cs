2f023ea5fa3216079c22809ad6861c42c91b1a5e
Tatiana Meshcheryakova <tania.meshcher@gmail.com>
2018-03-01 20:09:09 +0100

    Article parser created in the Model.

---
+++ WebApplication1/Model/ArticleHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model.Deserialization;
+
+namespace WebApplication1.Model
+{
+    public class ArticleHandler
+    {
+        private GoogleNews _googleNews;
+        public ArticleHandler(GoogleNews googleNews)
+        {
+            _googleNews = googleNews;
+        }
+
+        public void CountByKeyword(string keyword)
+        {
+            WordFinder wordFinder = new WordFinder();
+            if (_googleNews.articles == null)
+            {
+                throw new Exception("There are no articles available");
+            }
+            else
+            {
+                foreach (Article article in _googleNews.articles)
+                {
+                    if (SumAccumulation.CacheDictionary.ContainsKey(article.url))
+                    {
+                        SumAccumulation.CacheDictionary[article.url].Exists = true;
+                    }
+                    else
+                    {
+                        int count = wordFinder.Find(keyword, article.description);
+                        SumAccumulation.Sum += count;
+                        CountExists countExists = new CountExists(count, true);
+                        SumAccumulation.CacheDictionary.Add(article.url, countExists);
+                    }
+                }
+
+                foreach (var item in SumAccumulation.CacheDictionary.Where(a => !a.Value.Exists).ToList())
+                {
+                    SumAccumulation.CacheDictionary.Remove(item.Key);
+                }
+
+                foreach (string key in SumAccumulation.CacheDictionary.Keys)
+                {
+                    SumAccumulation.CacheDictionary[key].Exists = false;
+                }
+
+            }
+
+        }
+    }
+}
